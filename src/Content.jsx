5b6465f585807f064de4bca21c84785a4f7f529a
Anna N Helms <unicornbuddha@gmail.com>
2025-01-29 18:06:24 -0800

    passes placeholder data to the index component as props in Content file and maps through array given props in RecipesIndex file

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,14 @@
 import { RecipesIndex } from "./RecipesIndex";
 
 export function Content() {
+  const recipes = [
+    {id: 1, title: "Raw Eggs", prep_time: 100, servings: 1, description: "2 raw eggs in shell", ingredients: "2 eggs", directions: "Crack open two egg shells", chef: "Anna Helms", image_url: "https://greenlifeinsocal.com/wp-content/uploads/2010/02/bc-bridge-eggs-040-first-two-eggs.jpg"},
+    {id: 2, title: "Raw Eggs", prep_time: 100, servings: 1, description: "2 raw eggs in shell", ingredients: "2 eggs", directions: "Crack open two egg shells", chef: "Anna Helms", image_url: "https://greenlifeinsocal.com/wp-content/uploads/2010/02/bc-bridge-eggs-040-first-two-eggs.jpg"}
+  ];
+
   return (
     <div>
-      <RecipesIndex />
+      <RecipesIndex recipes={recipes} />
     </div>
   )
 }
--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -1,7 +1,36 @@
-export function RecipesIndex() {
+import { useState } from "react";
+export function RecipesIndex(props) {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div>
+    <div id="recipes-index" className="container text-center">
       <h1>All recipes</h1>
+      Search Recipes:{" "}
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+        list="recipe-titles"
+      />
+      <datalist id="recipe-titles">
+        {props.recipes.map((recipe) => (
+          <option key={recipe.id}>{recipe.title}</option>
+        ))}
+      </datalist>
+      <div className="row">
+        {props.recipes
+          .filter((recipe) => recipe.title.toLowerCase().includes(searchTerm.toLowerCase()))
+          .map((recipe) => (
+            <div key={recipe.id} className="recipes col-lg-4 col-md-6 col-12 my-3">
+              <h4>{recipe.title}</h4>
+              <img src={recipe.image_url} alt="" />
+              <div>Chef: {recipe.chef}</div>
+              <button className="btn btn-secondary" onClick={() => props.onShowRecipe(recipe)}>
+                More info
+              </button>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }

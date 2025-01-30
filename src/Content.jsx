fc1cf56f572057ed62b617c851ace942b6930a03
Anna N Helms <unicornbuddha@gmail.com>
2025-01-29 21:27:09 -0800

    Adds PhotosNew file that returns a form and imports that file to Content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,22 +1,22 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { RecipesIndex } from "./RecipesIndex";
+import { RecipesNew } from "./RecipesNew"
 
 export function Content() {
   const [recipes, setRecipes] = useState([]);
 
   const handleIndexRecipes = () => {
-    console.log("handleIndexRecipes");
     axios.get("http://localhost:3000/recipes.json").then((response) => {
-      console.log(response.data);
       setRecipes(response.data);
     });
   };
 
   useEffect(handleIndexRecipes, []);
-  
+
   return (
     <div>
+      <RecipesNew />
       <RecipesIndex recipes={recipes} />
     </div>
   )
---
+++ src/RecipesNew.jsx
@@ -0,0 +1,72 @@
+export function RecipesNew() {
+  return (
+    <>
+        <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-xl mt-20">
+          <h1 className="text-xl font-bold text-black capitalize light:text-black">New Recipe</h1>
+            <form>
+                <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                    <div>
+                        <label className="text-black light:text-black-200">Title</label>
+                        <input id="title" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md light:bg-white-800 light:text-black-300 light:border-gray-600 focus:border-blue-300 light:focus:border-blue-300 focus:outline-none focus:ring" />
+                    </div>
+
+                    <div>
+                        <label className="text-black light:text-black-200">Chef</label>
+                        <input id="chef" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md light:bg-white-800 light:text-black-300 light:border-gray-600 focus:border-blue-300 light:focus:border-blue-300 focus:outline-none focus:ring" />
+                    </div>
+
+                    <div>
+                        <label className="text-black light:text-gray-200">Description</label>
+                        <textarea id="description" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md light:bg-gray-800 light:text-gray-300 light:border-gray-600 focus:border-blue-300 light:focus:border-blue-300 focus:outline-none focus:ring"></textarea>
+                    </div>
+
+                    <div>
+                        <label className="text-black light:text-gray-200">Ingredients</label>
+                        <textarea id="ingredients" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md light:bg-gray-800 light:text-gray-300 light:border-gray-600 focus:border-blue-300 light:focus:border-blue-300 focus:outline-none focus:ring"></textarea>
+                    </div>
+
+                    <div>
+                        <label className="text-black light:text-gray-200">Directions</label>
+                        <textarea id="directions" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md light:bg-gray-800 light:text-gray-300 light:border-gray-600 focus:border-blue-300 light:focus:border-blue-300 focus:outline-none focus:ring"></textarea>
+                    </div>
+
+                    <div className="flex gap-1">
+                        <div>
+                            <label className="text-black light:text-black-200">Prep Time</label>
+                            <input id="prep_time" type="number" className="block px-3 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md light:bg-white-800 light:text-black-300 light:border-gray-600 focus:border-blue-300 light:focus:border-blue-300 focus:outline-none focus:ring" />
+                        </div>
+                        <div>
+                            <label className="text-black light:text-black-200">Servings</label>
+                            <input id="servings" type="number" className="block px-3 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md light:bg-white-800 light:text-black-300 light:border-gray-600 focus:border-blue-300 light:focus:border-blue-300 focus:outline-none focus:ring" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label className="text-black light:text-black-200">Image Url or Upload</label>
+
+                        <input id="image_url" type="url" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md light:bg-white-800 light:text-black-300 light:border-gray-600 focus:border-blue-300 light:focus:border-blue-300 focus:outline-none focus:ring" />
+
+                        <div className="space-y-1 text-center">
+                            <div className="flex text-sm text-black-600 m-2">
+                                <label className="relative cursor-pointer bg-white border border-black-700 rounded-md font-medium text-blue-800 hover:text-blue-800 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-800">
+                                    <span className="m-2">Upload a file</span>
+                                        
+                                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                                </label>
+                                    <p className="ml-3 text-black">
+                                        PNG, JPG, GIF up to 10MB
+                                    </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex justify-end mt-6 space-x-5">
+                    <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-green-500 rounded-md hover:bg-green-900 focus:outline-none focus:bg-gray-600">Update</button>
+                    <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-900 focus:outline-none focus:bg-gray-600">Save</button>
+                </div>
+            </form>
+        </section>
+    </>
+  );
+}

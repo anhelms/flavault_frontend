00dbe8a70abadcf64940ab5d7d3a310943ff110d
Anna N Helms <unicornbuddha@gmail.com>
2025-01-29 18:11:52 -0800

    Adds useState and useEffect hooks to define the data via the backend web request

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,20 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { RecipesIndex } from "./RecipesIndex";
 
 export function Content() {
-  const recipes = [
-    {id: 1, title: "Raw Eggs", prep_time: 100, servings: 1, description: "2 raw eggs in shell", ingredients: "2 eggs", directions: "Crack open two egg shells", chef: "Anna Helms", image_url: "https://greenlifeinsocal.com/wp-content/uploads/2010/02/bc-bridge-eggs-040-first-two-eggs.jpg"},
-    {id: 2, title: "Raw Eggs", prep_time: 100, servings: 1, description: "2 raw eggs in shell", ingredients: "2 eggs", directions: "Crack open two egg shells", chef: "Anna Helms", image_url: "https://greenlifeinsocal.com/wp-content/uploads/2010/02/bc-bridge-eggs-040-first-two-eggs.jpg"}
-  ];
+  const [recipes, setRecipes] = useState([]);
 
+  const handleIndexRecipes = () => {
+    console.log("handleIndexRecipes");
+    axios.get("http://localhost:3000/recipes.json").then((response) => {
+      console.log(response.data);
+      setRecipes(response.data);
+    });
+  };
+
+  useEffect(handleIndexRecipes, []);
+  
   return (
     <div>
       <RecipesIndex recipes={recipes} />
